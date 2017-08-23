87b8c4d369a0d8e7dc7d9cc20a3774e3386bb5f4
daijun lu <orange475@hotmail.com>
2017-08-22 23:20:59 -0700

    Swag-Shop
    Swag-Shop-Web and Swap-Shop-Api added

---
+++ Node/swag-shop-web/src/product-condensed/product-condensed.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import './product-condensed.css';
+import DataService from '../services/data-service';
+
+
+let ds = new DataService();
+class ProductCondensed extends Component {
+    constructor(props) {
+        super(props);
+        
+        //BIND
+        this.removeProduct = this.removeProduct.bind(this);
+    }
+    
+    removeProduct = () => {
+        ds.removeWishListItem(this.props.product);
+        
+    }
+    
+    render() {
+        return (
+            <li className = "list-group-item pc-condensed">
+                <a className = "btn btn-outline-danger" onClick = {() => this.removeProduct()}>X</a>
+                <p>{this.props.product.title} | ${this.props.product.price}</p>
+                
+                
+            </li>
+        );
+        
+    }
+}
+
+export default ProductCondensed;
